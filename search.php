e4a315de34dfa1b8207956d9fbe430199710f0ce
Natalie Tam <30819550+nataliet57@users.noreply.github.com>
2020-01-16 01:11:43 -0500

    Add files via upload
    first commit

---
+++ search.php
@@ -0,0 +1,89 @@
+<?php
+
+	if(isset($_GET["term"])) {
+		$term = $_GET["term"];
+	}
+	else {
+		exit("You must enter a search term");
+	}
+
+	$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper">
+	
+		<div class="header">
+
+
+			<div class="headerContent">
+
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/googleLogo.png">
+					</a>
+				</div>
+
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<div class="searchBarContainer">
+
+							<input class="searchBox" type="text" name="term">
+							<button class = "searchButton">
+								<img src="assets/images/searchIcon.png">
+							</button>
+						</div>
+
+
+
+
+					</form>
+
+
+				</div>
+
+
+
+			</div>
+			<a href = "#"> </a>
+			<div class="tabsContainer">
+
+				<ul class="tabList">
+
+					<li class = "<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li class = "<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
+
+
+			</div>
+
+
+
+		</div>
+	</div>
+
+</body>
+</html>
